7ab3bad14988ce525b87501871f592f1ed566caa
haorein <donghao0212@gmail.com>
2023-04-12 17:36:11 +0200

    add react project as frontend

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = '/static/'
+STATIC_URL = '/api-static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = "/media/"
